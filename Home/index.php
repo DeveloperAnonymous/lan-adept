27b973c059d2594d5991c1056b0cc334b5b87af0
NullException <felixbourdages@gmail.com>
2020-02-13 20:32:50 -0500

    Maintenant ça marche

--- Home/index.php
+++ Home/index.php
@@ -1,10 +1,15 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-
+    <title>LAN ADEPT</title>
+    <link type="text/css" rel="stylesheet" href="../Assets/css/bootstrap.css" />
+    <link type="text/css" rel="stylesheet" href="../Assets/css/index.css" />
 </head>
 <body>
 
+<script src="../Assets/js/jquery-3.4.1.min.js"></script>
+<script src="../Assets/js/popper.min.js"></script>
+<script src="../Assets/js/bootstrap.js"></script>
 </body>
 </html>
 
