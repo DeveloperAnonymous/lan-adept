436140e8d1179d589b52ad61d92429fdf8b57af4
DeveloperAnonymous <matthew_quebec@outlook.fr>
2020-02-13 20:53:29 -0500

    new updates

--- Home/index.php
+++ Home/index.php
@@ -4,12 +4,32 @@
     <title>LAN ADEPT</title>
     <link type="text/css" rel="stylesheet" href="../Assets/css/bootstrap.css" />
     <link type="text/css" rel="stylesheet" href="../Assets/css/index.css" />
+    <link type="text/css" rel="stylesheet" href="../Assets/css/style.css" />
 </head>
-<body>
+<body style="background-image: url('../Assets/img/adeptInfo.jpg');">
+    <header id="header">
+        <div class="container-fluid">
+            <div id="logo" class="pull-left">
+                <h1><a href="#" class="scrollto">ADEPT</a></h1>
+            </div>
+            <nav id="nav-menu-container">
+                <ul class="nav-menu sf-js-enabled sf-arrows" style="touch-action: pan-y;">
+                    <li class="menu-active d-block d-sm-none"><a href="#">Accueil</a></li>
+                    <li><a href="/?lang=fr#about">Qui sommes nous</a></li>
+                    <li><a href="/?lang=fr#services">Nos services</a></li>
+                    <li><a href="/?lang=fr#team">Membres du conseil</a></li>
+                    <li><a href="/?lang=fr#contact">Contact</a></li>
+                    <li><a href="http://adeptinfo.ca/lan">LAN de L'ADEPT</a></li>
+                    <li><a href="?lang=en">en</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 
-<script src="../Assets/js/jquery-3.4.1.min.js"></script>
-<script src="../Assets/js/popper.min.js"></script>
-<script src="../Assets/js/bootstrap.js"></script>
+    <!-- Bootstrap JS File -->
+    <script src="../Assets/js/jquery-3.4.1.min.js"></script>
+    <script src="../Assets/js/popper.min.js"></script>
+    <script src="../Assets/js/bootstrap.js"></script>
 </body>
-</html>
 
+</html>
