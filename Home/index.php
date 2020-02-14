3b63283c8b9f778555bcf7abcf81c9db2b9893dc
DeveloperAnonymous <matthew_quebec@outlook.fr>
2020-02-13 21:43:26 -0500

    Finition du layout de base

--- Home/index.php
+++ Home/index.php
@@ -2,32 +2,90 @@
 <html>
 <head>
     <title>LAN ADEPT</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="../Assets/css/bootstrap.css" />
-    <link type="text/css" rel="stylesheet" href="../Assets/css/index.css" />
-    <link type="text/css" rel="stylesheet" href="../Assets/css/style.css" />
-</head>
-<body style="background-image: url('../Assets/img/adeptInfo.jpg');">
-    <header id="header">
-        <div class="container-fluid">
-            <div id="logo" class="pull-left">
-                <h1><a href="#" class="scrollto">ADEPT</a></h1>
-            </div>
-            <nav id="nav-menu-container">
-                <ul class="nav-menu sf-js-enabled sf-arrows" style="touch-action: pan-y;">
-                    <li class="menu-active d-block d-sm-none"><a href="#">Accueil</a></li>
-                    <li><a href="">Places</a></li>
-                    <li><a href="">Se connecter</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet">
 
-    <!-- Bootstrap JS File -->
-    <script src="../Assets/js/jquery-3.4.1.min.js"></script>
-    <script src="../Assets/js/popper.min.js"></script>
-    <script src="../Assets/js/bootstrap.js"></script>
+    <!-- Stylesheets -->
+    <link type="text/css" rel="stylesheet" href="../Assets/css/bootstrap.css"/>
+    <link type="text/css" rel="stylesheet" href="../Assets/css/index.css"/>
+    <link type="text/css" rel="stylesheet" href="../Assets/css/style.css"/>
+</head>
+<body>
+<header id="header">
+    <div class="container-fluid">
+        <div id="logo" class="pull-left">
+            <h1><a href="#" class="scrollto">LAN ADEPT</a></h1>
+        </div>
+        <nav id="nav-menu-container">
+            <ul class="nav-menu sf-js-enabled sf-arrows" style="touch-action: pan-y;">
+                <li class="menu-active d-block d-sm-none"><a href="#">Accueil</a></li>
+                <li><a href="">Places</a></li>
+                <li><a href="">Se connecter</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<section id="intro">
+    <div class="intro-container">
+        <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#introCarousel" data-slide-to="0" class="active"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active" id="first" style="background-image: url('../Assets/img/adeptInfo.jpg');">
+                    <div class="carousel-container">
+                        <div class="carousel-content">
+                            <h2>Bienvenue à l'ADEPT</h2>
+                            <p>L'ADEPT, c'est l'Association Des Étudiants et Étudiantes du Programme de Technique
+                                Informatique du cégep Édouard-Montpetit. <br class="hidden-mobile"><strong>Venez nous
+                                    voir, nous sommes au local F-045 situé dans la cafeteria !</strong>
+                            </p>
+                            <a href="#about" class="btn-get-started scrollto">Découvrir l'ADEPT</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- À décommenter pour ajouter des pages au carroussel
+                <div class="carousel-item" style="background-image: url('img/intro-carousel/2.jpg');">
+                    <div class="carousel-container">
+                      <div class="carousel-content">
+                          <h2>Bienvenue à l'ADEPT</h2>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                          <a href="#about" class="btn-get-started scrollto">Découvrir l'ADEPT</a>
+                      </div>
+                    </div>
+                    </div>
+
+                    <div class="carousel-item" style="background-image: url('img/intro-carousel/3.jpg');">
+                    <div class="carousel-container">
+                      <div class="carousel-content">
+                        <h2>Bienvenue à l'ADEPT</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <a href="#about" class="btn-get-started scrollto">Découvrir l'ADEPT</a>
+                      </div>
+                    </div>
+                    </div> -->
+                <!-- </div>
+                <a class="carousel-control-prev" href="#introCarousel" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon ion-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Précédent</span>
+                </a>
+                <a class="carousel-control-next" href="#introCarousel" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon ion-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Suivant</span>
+                </a>
+              </div> -->
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Bootstrap JS File -->
+<script src="../Assets/js/jquery-3.4.1.min.js"></script>
+<script src="../Assets/js/popper.min.js"></script>
+<script src="../Assets/js/bootstrap.js"></script>
 </body>
 
 </html>
