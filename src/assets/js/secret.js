584ffbab083a41796af0d53f509dc9ef7a200f05
Duteau Matthew <1774134@cegepmontpetit.ca>
2020-02-17 16:26:47 -0500

    modifs navbar

---
+++ src/assets/js/secret.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+        n = 0;
+    $(document).keydown(function (e) {
+        if (e.keyCode === k[n++]) {
+            if (n === k.length) {
+
+                $("<link/>", {
+                    rel: "stylesheet",
+                    type: "text/css",
+                    href: "assets/css/secret.css"
+                }).appendTo("head");
+
+                $('img#footer-logo').attr("src","assets/img/badgeHS.png");
+
+                new Audio('assets/audio/secret.mp3').play();
+
+                n = 0;
+                return false;
+            }
+        }
+        else {
+            n = 0;
+        }
+    });
+});
